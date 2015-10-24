61afd0e03a15ed12e16cf4f85016fa92feb95792
ferg <me@ferg.in>
2015-10-24 14:47:56 +0600

    loading controller in controllers loader

--- libs/controllersloader.class.php
+++ libs/controllersloader.class.php
@@ -17,7 +17,22 @@ class ControllersLoader {
             }
         }
 
-        exit;
+        array_push(
+            self::$stack,
+            array(
+            'controller' => $info['controller']['controller'],
+                'action' => $info['action']['action'],
+            )
+        );
+
+        $class = new $info['controller']['class'];
+        $class->__callAction(
+            $info['action']['method'],
+            $info['action']['argumets'],
+            $info['args']
+        );
+
+        array_pop(self::$stack);
     }
 
     public static function exists($url) {
@@ -25,11 +40,92 @@ class ControllersLoader {
     }
 
     protected static function getController($url) {
-        $list = self::getControllers();
+        $controllers_list = self::getControllers();
 
-        print_r($list);
+        $is_valid = true;
+        $counter = 0;
+        $url_args = array();
+        $url_controllers = array();
+        $controller = '';
 
-        exit;
+        // split url into args and valid controllers list
+
+        foreach (explode('/', $url) as $part) {
+            if (!$part) {
+                continue;
+            }
+
+            ++$counter;
+            $url_args[] = $part;
+
+            if (!$is_valid) {
+                continue;
+            }
+
+            if (!preg_match('#^[A-Za-z][A-Za-z0-9_]*$#', $part)) {
+                $is_valid = false;
+                continue;
+            }
+
+            if ($controller) {
+                $controller .= '/';
+            }
+
+            $controller .= strtolower($part);
+
+            if (!isset($controllers_list[$controller])) {
+                continue;
+            }
+
+            $url_controllers[] = array(
+                'controller' => $controller,
+                'pos' => $counter,
+            );
+        }
+
+        // reverse found controllers and take the one best matching
+        $url_controllers = array_reverse($url_controllers);
+
+        $controller = '';
+        $controller_args = array();
+
+        foreach ($url_controllers as $c) {
+            if (!isset($controllers_list[$c['controller']])) {
+                continue;
+            }
+
+            // found one
+            $controller = $c['controller'];
+
+            // and arguments for that controller
+            for ($i = $c['pos'], $end = count($url_args); $i < $end; ++$i) {
+                $controller_args[] = $url_args[$i];
+            }
+
+            break;
+        }
+
+        if (!$controller) {
+            return false;
+        }
+
+        $info = $controllers_list[$controller];
+        $action = 'index';
+
+        // check if the first argument is an action
+        if (count($controller_args) && isset($info['actions'][strtolower($controller_args[0])])) {
+            $action = strtolower(array_shift($controller_args));
+        }
+
+        return array(
+            'controller' => array(
+                'file' => $info['file'],
+                'class' => $info['class'],
+                'controller' => $controller,
+            ),
+            'action' => $info['actions'][$action],
+            'args' => $controller_args,
+        );
     }
 
     protected static function getControllers() {
