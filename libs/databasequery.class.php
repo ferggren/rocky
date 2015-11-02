6fa554b6808a6f29d8fbf8eb0854c88eb31b4faf
ferg <me@ferg.in>
2015-11-02 15:18:18 +0600

    fix

--- libs/databasequery.class.php
+++ libs/databasequery.class.php
@@ -459,7 +459,7 @@ abstract class DatabaseQuery {
                 $query .= ', ';
             }
 
-            $query = $this->escapeFieldName($order_by['field']) . ' ' . $order_by['type'];
+            $query .= $this->escapeFieldName($order_by['field']) . ' ' . $order_by['type'];
         }
 
         return 'ORDER BY ' . $query;
