e36ff378dc69785d42ccbdeb1ec55021ceea5d70
ferg <me@ferg.in>
2016-01-17 00:23:13 +0600

    http cache disabler fix

--- system/common.php
+++ system/common.php
@@ -77,9 +77,9 @@ function decimal2ip($ip) {
 function disableBrowserCaching() {
     header('Expires: Thu, 19 Feb 1998 13:24:18 GMT');
     header('Last-Modified: '.gmdate("D, d M Y H:i:s").' GMT');
-    header('Cache-Control: no-cache, must-revelidate');
-    header('Cache-Control: post-check=0,pre-check=0');
-    header('Cache-Control: max-age=0');
+    header('Cache-Control: no-store, no-cache, must-revelidate');
+    header('Cache-Control: post-check=0, pre-check=0', false);
+    header('Cache-Control: max-age=0', false);
     header('Pragma: no-cache');
 }
 ?>
