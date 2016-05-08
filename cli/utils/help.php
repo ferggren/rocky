21ef3965a83ab27d678699347a78ec6a9b376dfd
ferg <me@ferg.in>
2016-05-08 18:55:08 +0600

    cli

---
+++ cli/utils/help.php
@@ -0,0 +1,102 @@
+<?php
+class UtilsHelp_CliController extends CliController {
+    public function action_default($script, $method) {
+        if ($script && $method) {
+            return self::show_script_method($script, $method);
+        }
+
+        if ($script) {
+            return self::show_script_methods($script);
+        }
+
+        return self::show_scripts();
+    }
+
+    protected static function show_script_method($script, $method) {
+        $script = strtolower($script);
+        $method = strtolower($method);
+
+        if (!is_array($scripts = CliControllersLoader::getControllers())) {
+            trigger_error("Unable to load scripts list");
+            exit;
+        }
+
+        if (!isset($scripts[$script])) {
+            trigger_error(sprintf(
+                "Script %s doesn't exists",
+                $script
+            ));
+            exit;
+        }
+
+        $script = $scripts[$script];
+
+        if (!isset($script['actions'][$method])) {
+            trigger_error(sprintf(
+                "Method %s:%s doesn't exists",
+                $script, $method
+            ));
+            exit;
+        }
+
+        $method = $script['actions'][$method];
+
+        printf(
+            "%s:%s arguments [%d]:",
+            $script['script'],
+            $method['action'],
+            count($method['arguments'])
+        );
+
+        foreach ($method['arguments'] as $argument) {
+            printf("\n --%s", $argument['name']);
+
+            if (!is_null($argument['value'])) {
+                printf(" [default = %s]", $argument['value']);
+            }
+        }
+    }
+
+    protected static function show_script_methods($script) {
+        $script = strtolower($script);
+
+        if (!is_array($scripts = CliControllersLoader::getControllers())) {
+            trigger_error("Unable to load scripts list");
+            exit;
+        }
+
+        if (!isset($scripts[$script])) {
+            trigger_error(sprintf(
+                "Script %s doesn't exists",
+                $script
+            ));
+            exit;
+        }
+
+        $script = $scripts[$script];
+
+        ksort($script['actions']);
+
+        printf("%s medhods [%d]:", $script['script'], count($script['actions']));
+
+        foreach ($script['actions'] as $action) {
+            printf("\n %s", $action['action']);
+        }
+    }
+
+    protected static function show_scripts() {
+        if (!is_array($scripts = CliControllersLoader::getControllers())) {
+            trigger_error("Unable to load scripts list");
+            exit;
+        }
+
+        ksort($scripts);
+
+        printf("Scripts list [%d]:", count($scripts));
+
+        foreach ($scripts as $script) {
+            printf("\n %s", $script['script']);
+        }
+    }
+}
+?>
--- libs/clicontrollersloader.class.php
+++ libs/clicontrollersloader.class.php
@@ -1,103 +1,140 @@
 <?php
 class CliControllersLoader {
-    protected static $scripts_list = false;
+    protected static $controllers_list = false;
 
-    public static function getScriptsList() {
-        if (is_array(self::$scripts_list)) {
-            return self::$scripts_list;
+    public static function loadController($controller_name, $controller_action = "default", $controller_args = array()) {
+        if (!($controller = self::getController($controller_name))) {
+            trigger_error(sprintf(
+                "Invalid controller %s\nUse ./cli.php utils/help to show all available scripts",
+                $controller_info['controller']
+            ));
+
+            exit;
         }
 
-        self::$scripts_list = array();
+        $controller_action = strtolower($controller_action);
 
-        $path = ROOT_PATH . '/cli/';
+        if (!self::controllerActionExists($controller_name, $controller_action)) {
+            trigger_error(sprintf(
+                "Action %s:%s doesn't exists\nUse ./cli.php utils/help %s to show all available methods",
+                $controller_name,
+                $controller_action,
+                $controller_name
+            ));
 
-        if (!is_dir($path)) {
+            exit;
+        }
+
+        if (!class_exists($controller['class'], false)) {
+            include(ROOT_PATH . '/cli/' . $controller['file']);
+
+            if (!class_exists($controller['class'], false)) {
+                trigger_error(sprintf(
+                    "Controller %s can't be found in %s",
+                    $controller['class'], $controller['file']
+                ));
+                exit;
+            }
+        }
+
+        $class = new $controller['class'];
+        $class->__callAction(
+            $controller['actions'][$controller_action]['method'],
+            $controller['actions'][$controller_action]['arguments'],
+            $controller_args
+        );
+    }
+
+    public static function controllerExists($controller_name) {
+        return !!self::getController($controller_name);
+    }
+
+    public static function controllerActionExists($controller_name, $action_name) {
+        if (!($controller = self::getController($controller_name))) {
             return false;
         }
 
-        if (!($dir = opendir($path))) {
+        if (!isset($controller['actions'][strtolower($action_name)])) {
             return false;
         }
 
-        while ($file = readdir($dir)) {
-            if ($file == '.' || $file == '..') {
-                continue;
-            }
-
-            if (is_dir($path.$file)) {
-                continue;
-            }
-
-            if (!preg_match('#^([a-z][a-zA-Z0-9]*)\.php$#', $file, $data)) {
-                continue;
-            }
-
-            $script = strtolower($data[1]);
-
-            if (isset(self::$scripts_list[$script])) {
-                continue;
-            }
-
-            if (!is_array($methods = self::processScript($file, $script))) {
-                continue;
-            }
-
-            self::$scripts_list[$script] = $methods;
-        }
-
-        return self::$scripts_list;
+        return true;
     }
 
-    protected static function processScript($file, $script_name) {
-        include(ROOT_PATH . '/cli/' . $file);
+    public static function getController($controller) {
+        $controller = strtolower($controller);
+        $controllers = self::getControllers();
 
-        $class = $script_name . '_cli';
-
-        if (!class_exists($class, false)) {
+        if (!isset($controllers[$controller])) {
             return false;
         }
 
-        $ref = new ReflectionClass($class);
-        return self::getScriptMethods($ref);
+        return $controllers[$controller];
     }
 
-    protected static function getScriptMethods($class_ref) {
-        $methods = array();
-
-        foreach ($class_ref->getMethods() as $method) {
-            $name = $method->name;
-
-            if (!$method->isPublic()) {
-                continue;
-            }
-
-            if (!$method->isStatic()) {
-                continue;
-            }
-
-            $methods[$name] = self::getMethodArguments($method);
+    public static function getControllers() {
+        if (is_array(self::$controllers_list)) {
+            return self::$controllers_list;
         }
 
-        return $methods;
+        if (is_array($controllers = self::loadFromFileCache())) {
+            return self::$controllers_list = $controllers;
+        }
+
+        if (!is_array($controllers = CliControllersParser::parse())) {
+            trigger_error('Unable to load controllers list');
+            exit;
+        }
+
+        self::$controllers_list = $controllers;
+        self::saveToFileCache(self::$controllers_list);
+
+        return self::$controllers_list;
     }
 
-    protected static function getMethodArguments($method) {
-        $args = array();
-
-        foreach ($method->getParameters() as $arg) {
-            $value = NULL;
-
-            if ($arg->isDefaultValueAvailable()) {
-                $value = $arg->getDefaultValue();
-            }
-
-            $args[] = array(
-                'name' => $arg->name,
-                'value' => $value,
-            );
+    public static function rebuildCache() {
+        if (!is_array($controllers = CliControllersParser::parse())) {
+            trigger_error('Unable to load controllers list');
+            exit;
         }
 
-        return $args;
+        self::saveToFileCache($controllers);
+    }
+
+    protected static function loadFromFileCache() {
+        if (!Config::get('app.cache_scripts')) {
+            return false;
+        }
+
+        if (!file_exists($tmp_path = ROOT_PATH . '/tmp/scripts.php')) {
+            return false;
+        }
+
+        include $tmp_path;
+
+        return isset($cache) ? $cache : false;
+    }
+
+    protected static function saveToFileCache($cache) {
+        if (!Config::get('app.cache_scripts')) {
+            return false;
+        }
+
+        $code = variable2code($cache);
+
+        if (!($file = fopen(ROOT_PATH . '/tmp/scripts.php.tmp', 'wb'))) {
+            return false;
+        }
+
+        fwrite($file, '<?php $cache = ' . $code . '; ?>');
+        fclose($file);
+
+        rename(
+            ROOT_PATH . '/tmp/scripts.php.tmp',
+            ROOT_PATH . '/tmp/scripts.php'
+        );
+
+        return true;
     }
 }
 ?>
