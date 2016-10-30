14d73d7c9c7805d9aaff52738fcc8761f27948ef
ferg <me@ferg.in>
2016-10-30 23:06:12 +0600

    rm lang

--- system/lang/en/ajax.php
+++
@@ -1,7 +0,0 @@
-<?php
-$strings = array(
-  'request_runtime_error'  => 'Request runtime error',
-  'request_timeout_error'  => 'Request timeout error',
-  'request_canceled_error' => 'Request canceled',
-);
-?>
--- system/lang/ru/ajax.php
+++
@@ -1,7 +0,0 @@
-<?php
-$strings = array(
-  'request_runtime_error'  => 'Ошибка выполнения запроса',
-  'request_timeout_error'  => 'Превышено время выполнения запроса',
-  'request_canceled_error' => 'Запрос отменен',
-);
-?>
